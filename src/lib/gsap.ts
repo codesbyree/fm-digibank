a3dfab7a65d05b69e3ed7d843af8c056439404f8
codesbyree <bahreesaepul1@gmail.com>
2026-09-01 20:54:10 +0700

    feature: mobile and desktop navigation

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -1,11 +1,10 @@
 import { gsap } from "gsap";
 
-import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 // ScrollSmoother requires ScrollTrigger
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { SplitText } from "gsap/SplitText";
 
-gsap.registerPlugin(Draggable, InertiaPlugin, ScrambleTextPlugin, ScrollTrigger, ScrollSmoother, SplitText, TextPlugin);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
-export { gsap, ScrambleTextPlugin, ScrollTrigger, ScrollSmoother, SplitText };
+export { gsap, ScrollTrigger, ScrollSmoother, SplitText };
